4b3b693f601b803c380595f91e375a1607ddfb1e
Hammad Khalid <hammadbink@gmail.com>
2023-01-20 17:49:44 +0500

    metrics dict output added

--- PythonAPI/pycocotools/cocoeval.py
+++ PythonAPI/pycocotools/cocoeval.py
@@ -456,37 +456,38 @@ class COCOeval:
             print(iStr.format(titleStr, typeStr, iouStr, areaRng, maxDets, mean_s))
             return mean_s
     
-        '''def _summarizeDets():
+
+        #ToDo: stats[0] = _summarize(1) calculates the average precision over all IoU thresholds, area ranges, 
+        # and maximum number of detections, but it doesn't calculate the mAP (mean average precision) across all classes.
+
+        #stats[6] = _summarize(0, maxDets=self.params.maxDets[0]) calculates the average recall over all IoU thresholds, area
+        #ranges, and a maximum number of detections specified,but it's not the mAR (mean average recall) across all classes.
+
+        def _summarizeDets():
             
-            mAP_050_095 = _summarize(1)
-            mAP_050 = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[2])
-            mAP_075 = _summarize(1, iouThr=.75, maxDets=self.params.maxDets[2])
-            AP_a_S = _summarize(1, areaRng='small', maxDets=self.params.maxDets[2])
-            AP_a_M = _summarize(1, areaRng='medium', maxDets=self.params.maxDets[2])
-            AP_a_L = _summarize(1, areaRng='large', maxDets=self.params.maxDets[2])
+            AP_050_095 = _summarize(1)
+            AP_050 = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[2])
+            AP_075 = _summarize(1, iouThr=.75, maxDets=self.params.maxDets[2])
+            #AP_a_S = _summarize(1, areaRng='small', maxDets=self.params.maxDets[2])
+            #AP_a_M = _summarize(1, areaRng='medium', maxDets=self.params.maxDets[2])
+            #AP_a_L = _summarize(1, areaRng='large', maxDets=self.params.maxDets[2])
             AR_MD_1 = _summarize(0, maxDets=self.params.maxDets[0])
             AR_MD_10 = _summarize(0, maxDets=self.params.maxDets[1])
             AR_MD_100 = _summarize(0, maxDets=self.params.maxDets[2])
-            AR_a_S = _summarize(0, areaRng='small', maxDets=self.params.maxDets[2])
-            AR_a_M = _summarize(0, areaRng='medium', maxDets=self.params.maxDets[2])
-            AR_a_L = _summarize(0, areaRng='large', maxDets=self.params.maxDets[2])
-            return stats'''
+            #AR_a_S = _summarize(0, areaRng='small', maxDets=self.params.maxDets[2])
+            #AR_a_M = _summarize(0, areaRng='medium', maxDets=self.params.maxDets[2])
+            #AR_a_L = _summarize(0, areaRng='large', maxDets=self.params.maxDets[2])
+            
+            metrics_dict = {
+                "ap50": AP_050,
+                "ap75": AP_075,
+                "ap5095": AP_050_095,
+                "ar1": AR_MD_1,
+                "ar10": AR_MD_10,
+                "ar100": AR_MD_100}
 
-        def _summarizeDets():
-            stats = np.zeros((12,))
-            stats[0] = _summarize(1)
-            stats[1] = _summarize(1, iouThr=.5, maxDets=self.params.maxDets[2])
-            stats[2] = _summarize(1, iouThr=.75, maxDets=self.params.maxDets[2])
-            stats[3] = _summarize(1, areaRng='small', maxDets=self.params.maxDets[2])
-            stats[4] = _summarize(1, areaRng='medium', maxDets=self.params.maxDets[2])
-            stats[5] = _summarize(1, areaRng='large', maxDets=self.params.maxDets[2])
-            stats[6] = _summarize(0, maxDets=self.params.maxDets[0])
-            stats[7] = _summarize(0, maxDets=self.params.maxDets[1])
-            stats[8] = _summarize(0, maxDets=self.params.maxDets[2])
-            stats[9] = _summarize(0, areaRng='small', maxDets=self.params.maxDets[2])
-            stats[10] = _summarize(0, areaRng='medium', maxDets=self.params.maxDets[2])
-            stats[11] = _summarize(0, areaRng='large', maxDets=self.params.maxDets[2])
-            return stats
+
+            return metrics_dict
 
         def _summarizeKps():
             stats = np.zeros((10,))
